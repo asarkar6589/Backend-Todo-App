78852fd7bc284f444d362cf5b97b9c65aeef9787
asarkar6589 <sarkararnab266@gmail.com>
2023-07-21 09:50:42 +0530

    changes

--- index.js
+++ index.js
@@ -16,12 +16,25 @@ config({
 app.use(express.json()); // for getting data from body.
 app.use(cookieParser()); // for geting the value of cookie from body.
 
-app.use(cors({
-    // origin: "http://localhost:3000",
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+// app.use(cors({
+//     // origin: "http://localhost:3000",
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }));
+
+// Allow requests from 'http://localhost:3000'
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Handle preflight requests
+app.options('*', (req, res) => {
+    res.status(200).end();
+});
 
 app.use(userRouter);
 app.use(taskRouter);
